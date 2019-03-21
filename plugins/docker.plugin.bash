8ac5082277e83dcf8e13bdc61682ebbf8b86a522
Tom Adamo <tadamo@buffalo.edu>
2019-03-21 08:48:24 -0400

    debug formatting

--- plugins/docker.plugin.bash
+++ plugins/docker.plugin.bash
@@ -1,25 +1,40 @@
 #!/usr/bin/env bash
 
-docker_stop_all_containers() {
-    docker stop "$(docker ps -aq)"
+docker-stop-all-containers() {
+    (
+        set -x
+        docker stop "$(docker ps -aq)"
+    )
 }
 
-docker_rm_all_containers() {
-    docker rm "$(docker ps -aq)"
+docker-rm-all-containers() {
+    (
+        set -x
+        docker rm "$(docker ps -aq)"
+    )
 }
 
-docker_rm_all_images() {
-    docker rmi "$(docker images -q)"
+docker-rm-all-images() {
+    (
+        set -x
+        docker rmi "$(docker images -q)"
+    )
 }
 
-docker_rm_all_networks() {
-    docker network rm "$(docker network ls -q)"
+docker-rm-all-networks() {
+    (
+        set -x
+        docker network rm "$(docker network ls -q)"
+    )
 }
 
 docker_rm_vm() {
     rm -rf ~/Library/Containers/com.docker.docker/Data/com.docker.driver.amd64-linux/Docker.qcow2
 }
 
-docker_system_prune_everything() {
-    docker system prune --force --all --volumes
+docker-system-prune-everything() {
+    (
+        set -x
+        docker system prune --force --all --volumes
+    )
 }
