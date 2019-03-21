5cdfdd804c6e5bd516aa4969cabddc66964e8159
Tom Adamo <tadamo@buffalo.edu>
2019-03-21 08:46:29 -0400

    formatting

--- lib/dircolors.bash
+++ lib/dircolors.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if [ -f ~/.dircolors/dircolors.ansi-dark ]; then
-   #eval $(dircolors -b ~/.dircolors/dircolors.ansi-dark)
-   eval $(TERM=xterm-256color bash -c 'gdircolors -b ~/.dircolors/dircolors.ansi-dark')
+    #eval $(dircolors -b ~/.dircolors/dircolors.ansi-dark)
+    eval $(TERM=xterm-256color bash -c 'gdircolors -b ~/.dircolors/dircolors.ansi-dark')
 elif [ -f ~tadamo/.dircolors/dircolors.ansi-dark ]; then
-   #eval $(dircolors -b ~tadamo/.dircolors/dircolors.ansi-dark)
-   eval $(TERM=xterm-256color bash -c 'gdircolors -b ~tadamo/.dircolors/dircolors.ansi-dark')
+    #eval $(dircolors -b ~tadamo/.dircolors/dircolors.ansi-dark)
+    eval $(TERM=xterm-256color bash -c 'gdircolors -b ~tadamo/.dircolors/dircolors.ansi-dark')
 fi
