aa5e95b6f906ffec7ec1cff4e9e8f50f08ecd7a7
Tom Adamo <tadamo@buffalo.edu>
2019-05-01 13:28:00 -0400

    updated

--- lib/perlbrew.bash
+++ lib/perlbrew.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 if [ -f ~/perl5/perlbrew/etc/bashrc ]; then
-   source ~/perl5/perlbrew/etc/bashrc
-   ln -s -f $(which perl) /usr/local/bin/perl
+    source ~/perl5/perlbrew/etc/bashrc
+    perlbrew use perl-5.28.1
+    ln -s -f $(which perl) /usr/local/bin/perl
 fi
